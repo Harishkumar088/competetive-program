0051eeb231e9565962d039c5ffc3c7c9c51804dc
Harishkumarrrr <31204450+Harishkumarrrr@users.noreply.github.com>
2018-06-27 16:53:42 +0530

    Update stock.java

--- week_1/day_1/stock.java
+++ week_1/day_1/stock.java
@@ -1,31 +1,86 @@
 # competetive-program
-class stockprice {
-	public static void main(String[] args) {
-		int arr[]={10,7,5,8,11,9};
-		int firstprice=arr[0];
-		int firstprofit=arr[1]-arr[0];
-		// int secondprofit=0;
-		for(int i=1;i<arr.length;i++){
+import org.junit.Test;
+import org.junit.runner.JUnitCore;
+import org.junit.runner.Result;
+import org.junit.runner.notification.Failure;
 
-			int secondprice=arr[i];
-			// System.out.println(secondprice+"hhhhhb");
+import static org.junit.Assert.*;
 
-			int secondprofit=secondprice-firstprice;
-			// System.out.println(secondprofit+"second");
+public class Solution {
 
-			
+    public static int getMaxProfit(int[] a) {
+        if (a.length < 2) {
+        throw new IllegalArgumentException("Getting a profit requires at least 2 prices");
+    }
 
-			firstprofit=Math.max(firstprofit,secondprofit);
-			// System.out.println(firstprofit+"llllll");
-
-
-			firstprice=Math.min(firstprice,secondprice);
-			// System.out.println(firstprice+"first");
-
+        // calculate the max profit
+        int minprice=a[0];
+		int maxprofit=a[1]-a[0];
+		for(int i=1;i<a.length;i++){
+			int minprice2=a[i];
+			int currentprofit=minprice2-minprice;
+			maxprofit=Math.max(maxprofit,currentprofit);
+			minprice=Math.min(minprice,minprice2);
 		}
-		// System.out.println(secondprofit);
-		System.out.println(firstprofit);
+		return maxprofit;
 
-	}
-	
+        
+    }
+
+    // tests
+
+    @Test
+    public void priceGoesUpThenDownTest() {
+        final int actual = getMaxProfit(new int[] {1, 5, 3, 2});
+        final int expected = 4;
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void priceGoesDownThenUpTest() {
+        final int actual = getMaxProfit(new int[] {7, 2, 8, 9});
+        final int expected = 7;
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void priceGoesUpAllDayTest() {
+        final int actual = getMaxProfit(new int[] {1, 6, 7, 9});
+        final int expected = 8;
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void priceGoesDownAllDayTest() {
+        final int actual = getMaxProfit(new int[] {9, 7, 4, 1});
+        final int expected = -2;
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void priceStaysTheSameAllDayTest() {
+        final int actual = getMaxProfit(new int[] {1, 1, 1, 1});
+        final int expected = 0;
+        assertEquals(expected, actual);
+    }
+
+    @Test(expected = Exception.class)
+    public void exceptionWithOnePriceTest() {
+        getMaxProfit(new int[] {5});
+    }
+
+    @Test(expected = Exception.class)
+    public void exceptionWithEmptyPricesTest() {
+        getMaxProfit(new int[] {});
+    }
+
+    public static void main(String[] args) {
+        Result result = JUnitCore.runClasses(Solution.class);
+        for (Failure failure : result.getFailures()) {
+            System.out.println(failure.toString());
+        }
+        if (result.wasSuccessful()) {
+            System.out.println("All tests passed.");
+        }
+    }
 }
