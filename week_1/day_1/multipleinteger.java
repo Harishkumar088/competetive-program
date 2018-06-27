8511f9963cc1b843448326d881cd1dbd07cf3174
Harishkumarrrr <31204450+Harishkumarrrr@users.noreply.github.com>
2018-06-27 16:54:51 +0530

    Update multipleinteger.java

--- week_1/day_1/multipleinteger.java
+++ week_1/day_1/multipleinteger.java
@@ -1,31 +1,100 @@
-class multiplyinteger{
-public static void main(String[] args) {
-	int p=1;
-	int a[]={1,3,7,4};
-	int b[]=new int[a.length];
-	for (int i=0;i<a.length;i++) {
-		b[i]=p;
-		System.out.println(b[i]);
-		p=a[i]*p;
-	}
+import org.junit.Test;
+import org.junit.runner.JUnitCore;
+import org.junit.runner.Result;
+import org.junit.runner.notification.Failure;
 
-		
-	
+import static org.junit.Assert.*;
 
+public class Solution {
 
-	int pr=1;
-	int c[]=new int[a.length];
-	for (int j=a.length-1;j>=1;j-- ) {
-		c[j]=b[j]*pr;
-		System.out.println(c[j]);
-		pr=pr*a[j];
-	}
+    public static int[] getProductsOfAllIntsExceptAtIndex(int[] a) {
 
+        // make an array of the products
+        int l=a.length;
+		int b[]=new int[l];
+		int c[]=new int[l];
+		int p=1;
+    	if (a.length < 2) {
+       throw new IllegalArgumentException("hi");
+       }
 
-	for (int k=0;k<c.length;k++ ) {
-		System.out.println(c[k]);
-		
-	}
-}
-	
+		for(int i=0;i<l;i++){
+			b[i]=p;
+			p=p*a[i];
+		}
+		p=1;
+		for(int j=l-1;j>=0;j--){
+			c[j]=p*b[j];
+			p=p*a[j];
+		}
+
+	/*for(int i=0;i<l;i++){
+		System.out.println(c[i]);
+	}*/
+
+        return c;
+    }
+    // tests
+
+    @Test
+    public void smallArrayTest() {
+        final int[] actual = getProductsOfAllIntsExceptAtIndex(new int[] {1, 2, 3});
+        final int[] expected = new int[] {6, 3, 2};
+        assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void longArrayTest() {
+        final int[] actual = getProductsOfAllIntsExceptAtIndex(new int[] {8, 2, 4, 3, 1, 5});
+        final int[] expected = new int[] {120, 480, 240, 320, 960, 192};
+        assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void arrayHasOneZeroTest() {
+        final int[] actual = getProductsOfAllIntsExceptAtIndex(new int[] {6, 2, 0, 3});
+        final int[] expected = new int[] {0, 0, 36, 0};
+        assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void arrayHasTwoZerosTest() {
+        final int[] actual = getProductsOfAllIntsExceptAtIndex(new int[] {4, 0, 9, 1, 0});
+        final int[] expected = new int[] {0, 0, 0, 0, 0};
+        assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void oneNegativeNumberTest() {
+        final int[] actual = getProductsOfAllIntsExceptAtIndex(new int[] {-3, 8, 4});
+        final int[] expected = new int[] {32, -12, -24};
+        assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void allNegativeNumbersTest() {
+        final int[] actual = getProductsOfAllIntsExceptAtIndex(new int[] {-7, -1, -4, -2});
+        final int[] expected = new int[] {-8, -56, -14, -28};
+        assertArrayEquals(expected, actual);
+    }
+
+    @Test(expected = Exception.class)
+    public void exceptionWithEmptyArrayTest() {
+        getProductsOfAllIntsExceptAtIndex(new int[] {});
+    }
+
+    @Test(expected = Exception.class)
+    public void exceptionWithOneNumberTest() {
+        getProductsOfAllIntsExceptAtIndex(new int[] {1});
+    }
+
+    public static void main(String[] args) {
+        Result result = JUnitCore.runClasses(Solution.class);
+        for (Failure failure : result.getFailures()) {
+            System.out.println(failure.toString());
+        }
+        if (result.wasSuccessful()) {
+            System.out.println("All tests passed.");
+        }
+    }
 }
