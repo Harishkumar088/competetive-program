66e2c40ddef05dcbe470d420836d064c2c37e551
Harishkumarrrr <31204450+Harishkumarrrr@users.noreply.github.com>
2018-07-09 16:52:51 +0530

    Create simulate5-sideded.py

---
+++ week3/day1/simulate5-sideded.py
@@ -0,0 +1,16 @@
+import random
+
+
+def rand7():
+    return random.randint(1, 7)
+
+
+def rand5():
+
+    # Implement rand5() using rand7()
+    while True:
+        k=rand7()
+        if k< 6:
+            return k
+print 'Rolling 5-sided die...'
+print rand5()
